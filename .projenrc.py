6bfe7b72a339c63e81a8b6a7d6b9f361a87b2aa1
Jacob Petterle <jacobpetterle@gmail.com>
2023-10-14 14:11:42 +0000

    chore: project created with projen

---
+++ .projenrc.py
@@ -0,0 +1,11 @@
+from projen.python import PoetryPyproject, PythonProject
+
+
+
+project = PythonProject(
+    name="nl-adapters",
+    module_name="nl_adapters",
+    author_name="Jacob Petterle",
+    author_email="jacobpetterle@tai-tutor.team",
+    version="0.0.0",
+)
