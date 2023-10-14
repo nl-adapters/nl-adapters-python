41f94d3daa0d43bc18819333219272c30139e0af
Jacob Petterle <jacobpetterle@gmail.com>
2023-10-14 23:12:05 +0000

    initial commit

--- .projenrc.py
+++ .projenrc.py
@@ -1,11 +1,17 @@
-from projen.python import PythonProject, VenvOptions
+from projen.python import PythonProject, VenvOptions, PoetryPyprojectOptionsWithoutDeps 
 
+
+AUTHORS = [
+    "Jacob Petterle",
+]
 project = PythonProject(
     author_email="jacobpetterle@tai-tutor.team",
-    author_name="Jacob Petterle",
+    author_name=AUTHORS[0],
     module_name="nl_adapters",
     name="nl-adapters",
     version="0.1.0",
+    poetry=True,
+    description="A library that provides natural language interfaces for any python code.",
 )
 
 project.synth()
